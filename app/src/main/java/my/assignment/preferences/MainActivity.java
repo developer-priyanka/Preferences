5eb984dba42f5909f646f32c64ac5641df62bb81
shafimirza <mirzashafibaig1989@gmail.com>
2016-07-30 00:07:24 +0530

    Initial commit

---
+++ app/src/main/java/my/assignment/preferences/MainActivity.java
@@ -0,0 +1,30 @@
+package my.assignment.preferences;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
